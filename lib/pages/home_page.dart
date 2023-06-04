466179361dd20f5ad5a3baea75994b5363d61a94
Leandro <atiladevelop@gmail.com>
2023-06-04 01:53:50 -0300

    cloud firestore working with the app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:the_wall/components/text_field.dart';
+import 'package:the_wall/components/wall_post.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,8 +15,29 @@ class _HomePageState extends State<HomePage> {
   // user
   final currentUser = FirebaseAuth.instance.currentUser!;
 
+  // text controller
+  final textController = TextEditingController();
+
   void signOut() {
     FirebaseAuth.instance.signOut();
+  }
+
+  // post message
+  void postMessage() {
+    // only post if there is something in the textfield
+    if (textController.text.isNotEmpty) {
+      // store in firebase
+      FirebaseFirestore.instance.collection("User Posts").add({
+        'UserEmail': currentUser.email,
+        'Message': textController.text,
+        'TimeStamp': Timestamp.now(),
+      });
+    }
+
+    // clear the textfield
+    setState(() {
+      textController.clear();
+    });
   }
 
   @override
@@ -36,11 +60,67 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             // the wall
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("User Posts")
+                    .orderBy("TimeStamp", descending: false)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        // get the message
+                        final post = snapshot.data!.docs[index];
+                        return WallPost(
+                          message: post['Message'],
+                          user: post['UserEmail'],
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        'Error: ${snapshot.error}',
+                      ),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
 
             // post message
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  // textfield
+                  Expanded(
+                    child: MyTextField(
+                      controller: textController,
+                      hintText: 'Write something on the wall',
+                      obscureText: false,
+                    ),
+                  ),
+
+                  // post button
+                  IconButton(
+                    onPressed: postMessage,
+                    icon: const Icon(Icons.arrow_circle_up),
+                  )
+                ],
+              ),
+            ),
 
             // logged in as
-            Text("Logged in as: ${currentUser.email!}")
+            Text(
+              "Logged in as: ${currentUser.email!}",
+              style: TextStyle(color: Colors.grey),
+            )
           ],
         ),
       ),
