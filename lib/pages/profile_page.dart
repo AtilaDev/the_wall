43effc40edfc059400d42d9bdbd27f05cabda4b1
Leandro <atiladevelop@gmail.com>
2023-06-05 23:33:00 -0300

    profile page implemented

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:the_wall/components/text_box.dart';
@@ -12,56 +13,144 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   // user
   final currentUser = FirebaseAuth.instance.currentUser!;
+  // all users
+  final usersCollection = FirebaseFirestore.instance.collection("Users");
+
+  // edit field
+  Future<void> editField(String field) async {
+    String newValue = "";
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.grey.shade900,
+        title: Text(
+          'Edit $field',
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        content: TextField(
+          autofocus: true,
+          style: const TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            hintText: "Enter new $field",
+            hintStyle: const TextStyle(color: Colors.grey),
+          ),
+          onChanged: (value) {
+            newValue = value;
+          },
+        ),
+        actions: [
+          // cancel button
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text(
+              'Cancel',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+
+          // save button
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(newValue),
+            child: const Text(
+              'Save',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+
+    // update in firestore
+    if (newValue.trim().isNotEmpty) {
+      await usersCollection.doc(currentUser.email).update({field: newValue});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade300,
-      appBar: AppBar(
-        title: const Text('Profile Page'),
-        backgroundColor: Colors.grey.shade900,
-      ),
-      body: ListView(
-        children: [
-          SizedBox(height: 50),
-          // profile pic
-          Icon(
-            Icons.person,
-            size: 72,
-          ),
+        backgroundColor: Colors.grey.shade300,
+        appBar: AppBar(
+          title: const Text('Profile Page'),
+          backgroundColor: Colors.grey.shade900,
+        ),
+        body: StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("Users")
+              .doc(currentUser.email)
+              .snapshots(),
+          builder: (context, snapshot) {
+            // get user date
+            if (snapshot.hasData) {
+              final userData = snapshot.data!.data() as Map<String, dynamic>;
 
-          SizedBox(height: 10),
+              return ListView(
+                children: [
+                  const SizedBox(height: 50),
+                  // profile pic
+                  const Icon(
+                    Icons.person,
+                    size: 72,
+                  ),
 
-          // user email
-          Text(
-            currentUser.email!,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.grey.shade700),
-          ),
+                  const SizedBox(height: 10),
 
-          SizedBox(height: 50),
+                  // user email
+                  Text(
+                    currentUser.email!,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey.shade700),
+                  ),
 
-          // user details
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: Text(
-              'My Details',
-              style: TextStyle(color: Colors.grey.shade600),
-            ),
-          ),
+                  const SizedBox(height: 50),
 
-          // username
-          MyTextBox(
-            text: 'FavreLeandro',
-            sectionName: 'username',
-            onPressed: () {},
-          )
+                  // user details
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Text(
+                      'My Details',
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                  ),
 
-          // bio
+                  // username
+                  MyTextBox(
+                    text: userData['username'],
+                    sectionName: 'username',
+                    onPressed: () => editField('username'),
+                  ),
 
-          // user posts
-        ],
-      ),
-    );
+                  // bio
+                  MyTextBox(
+                    text: userData['bio'],
+                    sectionName: 'bio',
+                    onPressed: () => editField('bio'),
+                  ),
+
+                  const SizedBox(height: 50),
+
+                  // user posts
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Text(
+                      'My Posts',
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                  ),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error ${snapshot.error}'),
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
   }
 }
